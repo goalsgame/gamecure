43d91ec3f65b404aa6417589dbcc812ca431205a
Jens Andersson <jens.andersson5@gmail.com>
2022-05-04 07:31:01 +0200

    Update checksum for longtail, change some error messages

--- src/Gamecure.Core/Longtail/ValidateLongtailChecksum.cs
+++ src/Gamecure.Core/Longtail/ValidateLongtailChecksum.cs
@@ -9,10 +9,10 @@ public class ValidateLongtailChecksum : IMiddleware<LongtailContext>
 {
     private static readonly Dictionary<Platform, string> _checksums = new()
     {
-        // Version v0.2.5, used Convert.ToHexString(SHA256.HashData(File.ReadAllBytes({longtailexecutable})))
-        { Platform.Linux, "2C897537F22BA8F79135953172151C6D8D99C1C3C26824EC43FED4CC63720C76" },
-        { Platform.Macos, "80E50088A8363BE6B7A30EA808364FE199D8A675C6103381A334B15899670DD5" },
-        { Platform.Windows, "01AB2087BC981AAC70E05613487A0120EE1C5F47B80BD4DCA3B083C0A3D1F9B7" }
+        // Version v0.3.0, used Convert.ToHexString(SHA256.HashData(File.ReadAllBytes({longtailexecutable})))
+        { Platform.Linux, "A4DE83D33D3FD15332671016CD4943AB5CD322A32481B4896F298890B0D913E5" },
+        { Platform.Macos, "842F19532980F808AD49FC1A265F77C6FBB9566D96A702F456858B844FE578EE" },
+        { Platform.Windows, "812BC0F9E25C05E57C2F3961CEDA1027BF442227792700F224E57A3CAF5DA742" }
     };
 
     public async Task<LongtailContext> OnInvoke(LongtailContext context, ContextDelegate<LongtailContext> next)
@@ -27,7 +27,7 @@ public class ValidateLongtailChecksum : IMiddleware<LongtailContext>
 
         if (context.LongtailPath == string.Empty)
         {
-            return context with { Failed = true, Reason = "LongtailPath is not set, cant' verify checksum" };
+            return context with { Failed = true, Reason = "LongtailPath is not set, can't verify checksum" };
         }
 
         var bytes = await File.ReadAllBytesAsync(context.LongtailPath);
@@ -38,7 +38,7 @@ public class ValidateLongtailChecksum : IMiddleware<LongtailContext>
         if (!checksumString.Equals(expectedChecksum))
         {
             Logger.Error($"Checksum mismatch. Expected {expectedChecksum}, got {checksumString}");
-            return context with { Failed = true, Reason = $"Checksum mismatch for longtail, please delete the executable at {context.LongtailPath}" };
+            return context with { Failed = true, Reason = "Checksum mismatch for longtail, go to settings page, check the Reset box under \"Download dependencies\" and press Download and Validate." };
         }
         Logger.Info("Checksum validation passed");
         return context;
