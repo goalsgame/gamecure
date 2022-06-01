cb29e7b25f2c379cbf18cd82c6c97ee9ce03b29b
rohit thomas <thomrohit@gmail.com>
2022-06-01 15:09:34 +0530

    slipt to get simplest solution

--- ci/helpers.groovy
+++ ci/helpers.groovy
@@ -30,32 +30,29 @@ def getAuthor() {
 		return bat(returnStdout: true, script: "git log -1 --pretty=format:'%%ae'").trim().readLines().drop(1).join(" ")
 }
 
-def buildStarted( String name, String platform = "") {
+def buildStarted( String name) {
     def map = getMap(name)
 	argoWorkflowHelper.triggerWorkflow("build-started-workflow", scmVarsHolder.GIT_BRANCH, map)
 }
 
-def buildFailed( String name, String platform = "") {
+def buildFailed( String name) {
 	def map = getMap(name)
 	argoWorkflowHelper.triggerWorkflow("build-failed-workflow", scmVarsHolder.GIT_BRANCH, map)
 }
 
 def buildSuccess(String name, List services = null, String namespace = "", String platform = "", String channel = "") {
 	def map = getMap(name, services, namespace)
-	echo map
 	argoWorkflowHelper.triggerWorkflow("build-success-workflow-template", scmVarsHolder.GIT_BRANCH, map)
 }
 
-def getbuildDescription(String name, List services = null, String namespace = "", String platform = ""){
-	if (platform)
-	return name +" ["+ platform +"]"
-	else
+def getbuildDescription(String name, List services = null, String namespace = ""){
 	return name
 }
 // maps key = variable name and value = data in variable (to get both name/value from the variable)
 def getMap(String name, List services = null, String namespace = ""){
 	def channel="",platform="" //being used in argo workflow so pass empty values
-	return [name: name, channel: channel, platform: platform,"buildDescription": getbuildDescription(name, services, namespace),
+	 String namestr = name.split(' ');
+	return [name: namestr, channel: channel, platform: platform,"buildDescription": getbuildDescription(name, services, namespace),
 		"version": getBuildId(), "branch": scmVarsHolder.GIT_BRANCH, "commit": getshortCommit(), "author": getAuthor(), "publishEvent": "false"]
 }
 // Needed to be able to import into Jenkinsfiles
